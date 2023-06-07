3d8f536ed46fb6143a6b2a83ce72bdad2ebcd4c0
devolo13 <devolo13@gmail.com>
2023-06-07 11:37:05 -0500

    commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+	public static void main(String[] args) {
+        String name = "Devin";
+        int age = 27;
+        String hometown = "Baytown";
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+	}
+}
